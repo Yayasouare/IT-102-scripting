dc40cda139be01fd6ddd799676ec3c00a46d7d3b
Yayasouare <Souare.Yaya@student.greenriver.edu>
2026-05-03 07:40:35 +0000

    Creating a read file script

--- start/CH04/ReadFile.py
+++ start/CH04/ReadFile.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 # Sample script that reads from a file
-# By 
+# By Yaya
+'''
+This is to read the file i created from the script writefile.py
+'''
+
+
+#Create a loop to open file and read its contents
+with open("hackme.txt", "r") as file:
+    contents = file.read()
+print("here is someone to hack - info dumb")
+print(contents)    
